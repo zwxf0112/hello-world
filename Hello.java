840c5eacc8d03ad3a81d7ce94197d9d21e024c42
zwxf0112 <939650079@qq.com>
2019-05-31 16:12:45 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
-public clas Hello{
-  public static main(String[] args){
-   System.out.println("Hello World!")
+public class Hello{
+  public static void main(String[] args){
+   System.out.println("Hello World!");
   }
 
 }
